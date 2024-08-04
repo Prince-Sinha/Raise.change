6210be392aa1edb4875c45e2e9d138dd08c816ec
Prince-Sinha <211114058@stu.manit.ac.in>
2024-08-05 03:26:01 +0530

    Fixing : Date Format

--- projectapp/src/pages/Department.jsx
+++ projectapp/src/pages/Department.jsx
@@ -29,6 +29,7 @@ export default function Department(){
     const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
     const day = String(date.getDate()).padStart(2, '0');
     const formattedDate = `${day}-${month}-${year}`;
+    
     return formattedDate;
  }
    useEffect(()=>{
--- projectapp/src/pages/Home.jsx
+++ projectapp/src/pages/Home.jsx
@@ -25,6 +25,7 @@ export default function Home(){
       const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
       const day = String(date.getDate()).padStart(2, '0');
       const formattedDate = `${day}-${month}-${year}`;
+      
       return formattedDate;
    }
 
